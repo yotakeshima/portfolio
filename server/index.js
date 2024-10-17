0a3b500d50958b87df75c2f45a6f1d23beb524cf
Yoshinori Takeshima <yotakeshima@gmail.com>
2024-10-17 16:02:20 -0400

    fetching repos

--- server/index.js
+++ server/index.js
@@ -5,6 +5,30 @@ import axios from 'axios';
 const app = express();
 
 app.use(cors());
+const user = 'yotakeshima';
+// const config = {
+//   headers: {
+//     Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+//     'Content-Type': 'application/json',
+//   },
+// };npm i
+
+app.get('/api/projects', async (req, res) => {
+  try {
+    const githubResponse = await axios.get(
+      `https://api.github.com/users/${user}/repos`
+    );
+
+    const repos = githubResponse.data.map((repo) => ({
+      name: repo.name,
+      description: repo.description,
+      url: repo.html_url,
+    }));
+    console.log(repos);
+  } catch (err) {
+    res.status(500).json({ error: 'Error fetching Github repos' });
+  }
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
