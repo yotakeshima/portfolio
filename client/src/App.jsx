0a81afd564d16787ebb81404b781ade3876820fa
Yoshinori Takeshima <yotakeshima@gmail.com>
2024-10-18 16:04:06 -0400

    added an experience component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,14 @@ import { useRef, useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Profile from './components/Profile';
 import AboutMe from './components/AboutMe';
+import Experience from './components/Experience';
 
 function App() {
   const [activeTab, setActiveTab] = useState(0);
 
-  const profileRef = useRef(null);
+  const experienceRef = useRef(null);
   const aboutRef = useRef(null);
-  const sections = [aboutRef, profileRef];
+  const sections = [aboutRef, experienceRef];
 
   const handleTabChange = (index) => {
     setActiveTab(index);
@@ -29,7 +30,7 @@ function App() {
           }
         });
       },
-      { threshold: 0.7 }
+      { threshold: 0.8 }
     );
 
     sections.forEach((section) => observer.observe(section.current));
@@ -41,11 +42,12 @@ function App() {
   return (
     <div>
       <Navbar activeTab={activeTab} onTabChange={handleTabChange} />
-      <div ref={profileRef} style={{ paddingTop: '100px' }}>
-        <Profile />
-      </div>
-      <div ref={aboutRef} style={{ paddingTop: '100px' }}>
+      <Profile />
+      <div ref={aboutRef}>
         <AboutMe />
+      </div>
+      <div ref={experienceRef}>
+        <Experience />
       </div>
     </div>
   );
